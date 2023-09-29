b7e8aa37b639176c3f7c90597bfa516d2cc4957d
mahouhou <freja_smith@hotmail.co.uk>
2023-09-29 19:56:50 +0100

    Restructured the project to extract the project submission form from the home page and organise it as its own function component. Updated the handleSubmit to redirect to the thankyou page using the useRouter hook.

--- app/page.js
+++ app/page.js
@@ -1,58 +1,5 @@
-"use client"
-
 import styles from './page.module.css'
-
-function Form() {
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    //access form element from the event object
-    //construct FormData object with form element
-    //store form data in formData object
-    const form = e.target
-    const formData = new FormData(form)
-
-    //reformat data to improve readability
-    const formObject = {}
-    formData.forEach((value, key) => {
-      formObject[key] = value
-    })
-
-    //log to console (or send to server)
-    console.log(formObject)
-
-    //redirect to thank you page
-    window.location.href="/thankyou"
-  }
-
-  return (
-    <>
-    <h1>Project Submission Form</h1>
-    <p>Fill out the form below to submit your project. Fields marked with an asterisk * are required.</p>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="fname">First name: *</label>
-      <input type="text" id="fname" name="fname" required aria-required="true"/>
-
-      <label htmlFor="lname">Last name: *</label>
-      <input type="text" id="lname" name="lname" required aria-required="true"/>
-
-      <label htmlFor="email">Email: *</label>
-      <input type="email" id="email" name="email" required aria-required="true"/>
-
-      <label htmlFor="project">Link your project: *</label>
-      <input type="url" id="project" name="project" placeholder="https://" required aria-required="true"/>
-
-      <label htmlFor="description">Describe your project: *</label>
-      <textarea id="description" name="description" required aria-required="true"/>
-
-      <label htmlFor="permission">Can we share your project on social media?</label>
-      <input type="checkbox" id="permission" name="permission" />
-
-      <button type="submit" value="Submit" id="submit">Submit</button>
-    </form>
-    </>
-  )
-}
+import Form from './forms/project-form/project-form'
 
 export default function Home() {
   return (
